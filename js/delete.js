42a47a4a54f4baaf0147dde471fb56d428dca7a5
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2023-11-17 20:01:26 -0300

    to fazeno

--- js/delete.js
+++ js/delete.js
@@ -1,21 +1,23 @@
 // selecionei o botão
-var btn_delete = document.querySelector('.del')
-btn_delete.addEventListener('click', _delete)
-
+var btn_delete = document.querySelector(".del");
+btn_delete.addEventListener("click", _delete);
 
 function _delete(event) {
-    event.preventDefault()
-    let produto = {id: 4226}
+  event.preventDefault();
+  let produto = { id: 4226 };
 
-    let url = "http://localhost/api_rest/back.php"
-    fetch(url,{
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(produto)
+  let url = "http://localhost/api_rest/back.php";
+  fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(produto),
+  })
+    .then(function (response) {
+      return response.json();
     })
-    .then(function (response) {return response.json()})
-    .then(function (response) {console.log(response)})
-    
+    .then(function (response) {
+      console.log(response);
+    });
 }
--- js/get.js
+++ js/get.js
@@ -1,16 +1,15 @@
-var btn_get = document.querySelector('.get')
-btn_get.addEventListener('click', _get)
+var btn_get = document.querySelector(".get");
+btn_get.addEventListener("click", _get);
 
-function _get(event){
-    event.preventDefault()
-    let url = "http://localhost/ConexaoMysql/select/selectPaciente.php"
+function _get(event) {
+  event.preventDefault();
+  let url = "http://localhost/ConexaoMysql/select/selectPaciente.php";
 
-    fetch(url)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (response) {
-            console.log(response)
-        })
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+    });
 }
-
